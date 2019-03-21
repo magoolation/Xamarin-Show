62dc01eaf898e0bdd44c598da97ab3082840e11a
Alexandre Santos Costa <alexandresantoscosta@gmail.com>
2019-03-21 14:15:11 -0300

    First visual interface dfinition

---
+++ XamarinShow.Accessibility/XamarinShow.Accessibility/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinShow.Accessibility
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private float ios12 = 82.3f;
+        private float ios11 = 10.5f;
+        private float other = 7.2f;
+
+        public float IOS12
+        {
+            get => ios12;
+            set => SetProperty(ref ios12, value);
+        }
+
+        public float IOS11
+        {
+            get => ios11;
+            set => SetProperty(ref ios11, value);
+        }
+
+        public float Other
+        {
+            get => other;
+            set => SetProperty(ref other, value);
+        }
+    }
+}
---
+++ XamarinShow.Accessibility/XamarinShow.Accessibility/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XamarinShow.Accessibility
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {            
+                field = value;
+                RaisePropertyChanged(propertyName); 
+        }
+    }
+}
